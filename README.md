458ffc7df5307dfff49622b46a71821bad480c1a
Andrw <kuvickdabest@gmail.com>
2022-10-20 04:01:34 -0300

    desktop design

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Frontendmentor Challenge
 
+![](challenge/design/desktop-preview.jpg)
 
 ## #2 Interactive rating component
 this is my second challenge from frontendmentor.io
 
 - [Source Frontendmentor Challenge](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI/hub/interactive-rating-component-83f1pihj6R)
-- [Live preview](https://fairstyle.github.io//)
+- [Live preview](https://fairstyle.github.io/frontendmentor-Interactive-rating-component)
 
 # Technologies
 - Html
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      // Using modern `hsl`
+      primary: 'hsl(var(--color-primary) / <alpha-value>)',
+      lightgray: 'hsl(var(--color-lightgray) / <alpha-value>)',
+      mediumgray: 'hsl(var(--color-mediumgray) / <alpha-value>)',
+      cleardarkblue: 'hsl(var(--color-cleardarkblue) / <alpha-value>)',
+      darkblue: 'hsl(var(--color-darkblue) / <alpha-value>)',
+      verydarkblue: 'hsl(var(--color-verydarkblue) / <alpha-value>)',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    }
   },
   plugins: [],
 }
